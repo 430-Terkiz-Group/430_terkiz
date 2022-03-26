4a8f590242cd56494c850ec3dfffecfb4a9d5da3
Bernard Challita <btc02@mail.aub.edu>
2022-03-26 17:34:43 +0200

    added gender and datetime joined

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
@@ -19,7 +21,7 @@ db = SQLAlchemy(app)
 meta = MetaData()
 # same as meta data
 userstable = Table('users', meta, Column('id', Integer, primary_key=True, autoincrement=True),
-                   Column('username', String, unique=True), Column('password', String),Column('mail',String),Column('dob',db.Date))
+                   Column('username', String, unique=True), Column('password', String),Column('mail',String),Column('dob',String),Column('gender',String),Column('date_joined',String))
 
 
 # user class
@@ -33,23 +35,28 @@ class User(db.Model):
     password = db.Column(db.String(128))
     mail = db.Column(db.String(128))
     dob = db.Column(db.String(100))
+    gender=Column(db.String(50))
+    date_joined = Column(db.String(50))
 
-    def __init__(self, username, password,mail,dob):
+    def __init__(self, username, password,mail,dob,gender):
         # self.id=id
         self.username = username
         self.password = bcrypt.generate_password_hash(password)
         self.mail = mail
         self.dob  = dob
+        self.gender=gender
+        self.date_joined=datetime.utcnow().strftime("%m/%d/%Y, %H:%M:%S")
 
 
 # api to add user to db
-# expects json file with username , password , mail and dob fields
+# expects json file with username , password , mail , dob and gender fields create time is automatic
 @app.route('/add_user', methods=['POST'])
 def add_user():
     name = request.json['username']
     pwd = request.json['password']
     mail = request.json['mail']
     dob = request.json['dob']
+    gender= request.json['gender']
     if not name or not pwd or not dob or not mail :
         # name is empty or pwd is empty
         abort(400)
@@ -58,12 +65,7 @@ def add_user():
     # similar username exists
     if not_unique:
         abort(406)  # not acceptable
-    newuser = User(name,pwd ,mail,dob)
+    newuser = User(name,pwd ,mail,dob,gender)
     db.session.add(newuser)
     db.session.commit()
     return "Success"
-#api to change user pwd
-#excpects username, old password(in case he just wants to change and did not forget it), new pwd, and type (1 for changing old password, 2 for changing pwd in case user forgot)
-
-
-
